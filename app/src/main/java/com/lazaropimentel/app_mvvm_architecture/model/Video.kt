5fd7dcba09528bfc70c3c8a744c909accf9a5483
Drlazinho <lazbonfim@hotmail.com>
2022-03-26 17:08:17 -0300

    model and retrofit

---
+++ app/src/main/java/com/lazaropimentel/app_mvvm_architecture/model/Video.kt
@@ -0,0 +1,8 @@
+package com.lazaropimentel.app_mvvm_architecture.model
+
+data class Video (
+    var title: String,
+    var author: String,
+    var thumbnailUrl: String,
+    var link: String
+)
---
+++ app/src/main/java/com/lazaropimentel/app_mvvm_architecture/service/RetrofitService.kt
@@ -0,0 +1,23 @@
+package com.lazaropimentel.app_mvvm_architecture.service
+
+import com.lazaropimentel.app_mvvm_architecture.model.Video
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface RetrofitService {
+    @GET("video.json")
+    fun getAllLives(): Call<List<Video>>
+
+    companion object {
+        private val retrofitService : RetrofitService by lazy {
+            val retrofit =  Retrofit.Builder()
+                .baseUrl("https://raw.githubusercontent.com/Drlazinho/video-api/main/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            retrofit.create(RetrofitService::class.java)
+        }
+    }
+}
